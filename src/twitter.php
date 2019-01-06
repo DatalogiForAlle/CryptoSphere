2640cc0db8bcd78ea2ee47a4fdc04eaa0b6110c3
Martin Dybdal <dybber@dybber.dk>
2019-01-06 12:00:19 +0100

    Twitter

---
+++ src/twitter.php
@@ -0,0 +1,15 @@
+<?php
+
+require "twitteroauth/autoload.php";
+require "twitter_auth_keys.php"
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+function tweet($message) {
+    $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+    $content = $connection->get("account/verify_credentials");
+    $status = $connection->post("statuses/update", ["status" => $tweet]);
+}
+
+
+?>
